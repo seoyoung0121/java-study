6fc5cf63d722e08cca256128495bc77bb94c461c
seoyoung0121 <88281319+seoyoung0121@users.noreply.github.com>
2024-11-26 10:55:54 +0900

    designpattern - feat: iterator

---
+++ designpattern-practices/src/main/java/behavioral/iterator/AggregateImpl.java
@@ -0,0 +1,30 @@
+package behavioral.iterator;
+
+public class AggregateImpl<E> implements Aggregate<E> {
+	private E[] datas;
+
+	public AggregateImpl(E[] datas) {
+		this.datas = datas;
+	}
+
+	@Override
+	public Iterator<E> createIterator() {
+		return new IteratorImpl();
+	}
+
+	private class IteratorImpl implements Iterator<E> {
+		private int index = 0;
+
+		@Override
+		public E next() {
+			return index < datas.length ? datas[index++] : null;
+		}
+
+		@Override
+		public boolean hasNext() {
+			return index < datas.length;
+		}
+
+	}
+
+}
