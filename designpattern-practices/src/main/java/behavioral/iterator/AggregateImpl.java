4a3ede1b36a7e15fc535d2db062bc379d58983b6
seoyoung0121 <88281319+seoyoung0121@users.noreply.github.com>
2024-11-26 11:08:10 +0900

    designpattern - docs: 주석 추가

--- designpattern-practices/src/main/java/behavioral/iterator/AggregateImpl.java
+++ designpattern-practices/src/main/java/behavioral/iterator/AggregateImpl.java
@@ -12,7 +12,7 @@ public class AggregateImpl<E> implements Aggregate<E> {
 		return new IteratorImpl();
 	}
 
-	private class IteratorImpl implements Iterator<E> {
+	private class IteratorImpl implements Iterator<E> {  //<E> 쓰면 앞에랑 충돌나서 헷갈림, <T>하면 갠춘, 근데 T로 참조하는 애가 없으니,,,
 		private int index = 0;
 
 		@Override
