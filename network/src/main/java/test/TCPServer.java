de377f8c75f4eca49dcfb63b2d766027b44a29a6
seoyoung0121 <88281319+seoyoung0121@users.noreply.github.com>
2024-11-28 09:52:26 +0900

    network - feat: TCP Server #2

--- network/src/main/java/test/TCPServer.java
+++ network/src/main/java/test/TCPServer.java
@@ -17,28 +17,43 @@ public class TCPServer {
 			// 2. 바인딩(binding)
 			// Socket에 InetSocketAddress[InetAddress(IPAddress) + port]를 바인딩 한다.
 			// IPAddress: 0.0.0.0: 특정 호스트 IP를 바인딩 하지 않는다. (다 열어놓는다)
-			serverSocket.bind(new InetSocketAddress("0.0.0.0", 5001));
+			serverSocket.bind(new InetSocketAddress("0.0.0.0", 60000));
 
 			// 3. accept
 			Socket socket = serverSocket.accept(); // blocking
 
-			System.out.println("연결 성공");
+			try {
+				InetSocketAddress inetRemoteSocketAddress = (InetSocketAddress) socket.getRemoteSocketAddress();
+				String remoteHostAddress = inetRemoteSocketAddress.getAddress().getHostAddress();
+				int remotePort = inetRemoteSocketAddress.getPort();
+				System.out.println("[server] connected by client [" + remoteHostAddress + ":" + remotePort + "]");
 
-			// 4. IO Stream 받아오기
-			InputStream is = socket.getInputStream();
+				// 4. IO Stream 받아오기
+				InputStream is = socket.getInputStream();
 
-			// 5. 데이터 읽기
-			byte[] buffer = new byte[256];
-			int readByteCount = is.read(buffer);
-			if (readByteCount == -1) {
-				// closed by client
-				System.out.println("[server] closed by client");
-				return;
+				// 5. 데이터 읽기
+				byte[] buffer = new byte[256];
+				int readByteCount = is.read(buffer);
+				if (readByteCount == -1) {
+					// closed by client
+					System.out.println("[server] closed by client");
+					return;
+				}
+
+				String data = new String(buffer, 0, readByteCount, "utf-8");
+				System.out.println("[server] receive: " + data);
+			} catch (IOException e) {
+				System.out.println("error: " + e);
+			} finally {
+				try {
+					if (socket != null && !socket.isClosed()) {
+						socket.close();
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+
 			}
-
-			String data = new String(buffer, 0, readByteCount, "utf-8");
-			System.out.println("[server] receive: " + data);
-
 		} catch (IOException e) {
 			System.out.println("[server] error: " + e);
 		} finally {
