f19442d4658da4a175410d8a63c2da132fe3cd6f
seoyoung0121 <88281319+seoyoung0121@users.noreply.github.com>
2024-12-31 11:04:30 +0900

    design pattern - template method

---
+++ designpattern-practices/src/main/java/behavioral/templatemethod/Client.java
@@ -0,0 +1,13 @@
+package behavioral.templatemethod;
+
+public class Client {
+	public static void main(String[] args){
+		new Calculator().add();
+		new Calculator().subtract();
+		
+		AbstractCalculate ac = new MultiplyCalculate();
+		ac.templateMethod();
+		ac = new DivideCalculate();
+		ac.templateMethod();
+	}
+}
--- network/src/main/java/echo/EchoClient.java
+++ network/src/main/java/echo/EchoClient.java
@@ -13,7 +13,7 @@ import java.net.SocketException;
 import java.util.Scanner;
 
 public class EchoClient {
-	private static final String SERVER_IP = "127.0.0.1";
+	private static final String SERVER_IP = "192.168.0.10";
 
 	public static void main(String[] args) {
 		Scanner scanner = null;
